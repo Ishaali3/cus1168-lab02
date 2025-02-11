94815c8c45c5fb8ebbab829c7eeea1b044e781c6
Ishaali3 <ishaxx20143@icloud.com>
2025-02-10 19:34:07 -0500

    Update Vehicle.java

--- src/main/java/academy/javapro/Vehicle.java
+++ src/main/java/academy/javapro/Vehicle.java
@@ -6,9 +6,29 @@ public abstract class Vehicle {
     protected int year;
     protected boolean isRunning;
 
-    // TODO: Add constructor
+    // Constructor
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isRunning = false;
+    }
 
-    // TODO: Add abstract methods
+    // Abstract methods
+    public abstract void startEngine();
+    public abstract void stopEngine();
+    public abstract void accelerate();
+    public abstract void brake();
 
-    // TODO: Add getters/setters
+    // Getters
+    public String getMake() { return make; }
+    public String getModel() { return model; }
+    public int getYear() { return year; }
+    public boolean isRunning() { return isRunning; }
+
+    // Setters
+    public void setMake(String make) { this.make = make; }
+    public void setModel(String model) { this.model = model; }
+    public void setYear(int year) { this.year = year; }
+    public void setRunning(boolean running) { this.isRunning = running; }
 }
